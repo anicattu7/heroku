9713dbd9234efb1e182f2af38ce648e464565dc9
Teena Anicattu Mathew <anicattu7@gmail.com>
2019-11-07 18:09:55 -0600

    commit

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
+import datetime
 
-app = Flask()
-
+app = Flask(__name__)
+year = datetime.datetime.now().year
+name = "Teena Mathew"
+cities = ["winnipeg", "edmonton", "toronto"]
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", year=year, name=name)
+
+
+@app.route('/about-me')
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port= 4000)
